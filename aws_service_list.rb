bb4dfc093f58ee5dfbf1259bf84b7965330d7c7b
Hidenori Miyoshi <miyohide@gmail.com>
2022-11-23 20:15:12 +0900

    parse JSON

--- aws_service_list.rb
+++ aws_service_list.rb
@@ -1,7 +1,8 @@
 require 'open-uri'
+require 'json'
 
 AWS_OFFER_INDEX_FILE = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json"
 
-URI.open(AWS_OFFER_INDEX_FILE) do |f|
-  f.each_line { |line| p line }
-end
+offer_index_json = JSON.parse(URI.open(AWS_OFFER_INDEX_FILE).read, symbolize_names: true)
+
+p offer_index_json
