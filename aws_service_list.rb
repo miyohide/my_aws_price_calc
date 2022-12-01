05ce812afe57b6d859e77c82f1104487ceebe311
Hidenori Miyoshi <miyohide@gmail.com>
2022-12-01 20:15:01 +0900

    create create_service_csv method

--- aws_service_list.rb
+++ aws_service_list.rb
@@ -14,30 +14,47 @@ def create_csv_url(base_url)
   AWS_OFFER_PREFIX + base_url.sub(/index.json$/, "#{AWS_TOKYO_REGION_CODE}/index.csv")
 end
 
+def create_service_csv(url, csv_file_name)
+  u = URI.parse(url)
+  http = Net::HTTP.new(u.host, u.port)
+  http.use_ssl = true
+  File.open(csv_file_name, 'w') do |f|
+    http.request_get(u.path) do |response|
+      response.read_body do |s|
+        f.write(s)
+      end
+    end
+  end
+end
+
 offer_index_json = JSON.parse(URI.parse(AWS_OFFER_INDEX_FILE).open.read, symbolize_names: true)
 
 result = offer_index_json[:offers].keys.each_with_object({}) do |item, memo_obj|
   memo_obj[remove_aws_amazon_prefix(item)] = create_csv_url(offer_index_json[:offers][item][:currentVersionUrl])
 end
 
-uri = URI.parse(result['EC2'])
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-File.open('EC2.csv', 'w') do |f|
-  http.request_get(uri.path) do |response|
-    response.read_body do |s|
-      f.write(s)
-    end
-  end
+['EC2', 'S3'].each do |s|
+  create_service_csv(result[s], s + '.csv')
 end
 
-uri = URI.parse(result['S3'])
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-File.open('S3.csv', 'w') do |f|
-  http.request_get(uri.path) do |response|
-    response.read_body do |s|
-      f.write(s)
-    end
-  end
-end
+# uri = URI.parse(result['EC2'])
+# http = Net::HTTP.new(uri.host, uri.port)
+# http.use_ssl = true
+# File.open('EC2.csv', 'w') do |f|
+#   http.request_get(uri.path) do |response|
+#     response.read_body do |s|
+#       f.write(s)
+#     end
+#   end
+# end
+#
+# uri = URI.parse(result['S3'])
+# http = Net::HTTP.new(uri.host, uri.port)
+# http.use_ssl = true
+# File.open('S3.csv', 'w') do |f|
+#   http.request_get(uri.path) do |response|
+#     response.read_body do |s|
+#       f.write(s)
+#     end
+#   end
+# end
